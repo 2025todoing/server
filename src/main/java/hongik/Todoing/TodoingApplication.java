a39b0229e1f80a0f12848994fb3a0f21ee15405c
hyesuhan <heasoo88@gmail.com>
2025-04-15 11:17:54 +0900

    Initial commit for Todoing Project!

---
+++ src/main/java/hongik/Todoing/TodoingApplication.java
@@ -0,0 +1,13 @@
+package hongik.Todoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongik/Todoing/TodoingApplicationTests.java
@@ -0,0 +1,13 @@
+package hongik.Todoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
