37dc2a96472af32ef5098efc64ab533fa34840cb
hyesuhan <heasoo88@gmail.com>
2025-09-27 10:45:06 +0900

    feat: pass 사용 로직 추가

---
+++ src/main/java/hongik/Todoing/domain/verification/repository/VerificationRepository.java
@@ -0,0 +1,10 @@
+package hongik.Todoing.domain.verification.repository;
+
+import hongik.Todoing.domain.verification.domain.Verification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VerificationRepository extends JpaRepository<Verification, Long> {
+
+
+
+}
