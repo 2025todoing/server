19674407dc852836636effbe4d0a1f330011dad5
hyesuhan <heasoo88@gmail.com>
2025-09-27 11:06:16 +0900

    feat: loging 전략

--- src/main/java/hongik/Todoing/domain/order/dto/pass/response/GetPassInfoResponse.java
+++ src/main/java/hongik/Todoing/domain/order/dto/pass/response/GetPassInfoResponse.java
@@ -1,7 +1,5 @@
 package hongik.Todoing.domain.order.dto.pass.response;
 
-import lombok.Getter;
 
-@Getter
 public record GetPassInfoResponse(Long passId, Integer usedCount, Integer limitCount, String itemName) {
 }
